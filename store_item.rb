55248f3868928596b9b189ce871eb3ebf83f856b
Thema Peters <tpete6957@gmail.com>
2018-08-20 21:40:39 -0500

    first days of work

---
+++ store_item.rb
@@ -0,0 +1,10 @@
+class Drinks
+
+  def initialize(milk, juice, water)
+    @milk = input_milk
+    @juice = input_juice
+    @water = input_water
+  end
+
+  def prints.info
+    puts "Buy #{ input_milk } and #{ input_juice } and #{ input_water}{"
